4d1cfb7c999c877f9a1a24c451a2c81c209cf244
Bensuo <500018262@live.wcs.ac.uk>
2017-02-23 20:07:40 +0000

    Adds md2 model loading libraries, loads test model

--- RT3DProject/RT3DProject/main.cpp
+++ RT3DProject/RT3DProject/main.cpp
@@ -6,8 +6,15 @@
 #include <glm/gtc/type_ptr.hpp>
 #include <stack>
 #include <stdio.h>
-GLuint shaderProgram;
+#include "md2model.h"
 
+#define DEG_TO_RADIAN 0.017453293
+
+GLuint shaderProgram;
+GLuint meshObjects[1];
+std::stack<glm::mat4> mvStack;
+md2model testModel;
+GLuint md2VertCount = 0;
 SDL_Window * setupRC(SDL_GLContext &context)
 {
 	SDL_Window * window;
@@ -40,8 +47,9 @@ SDL_Window * setupRC(SDL_GLContext &context)
 void init(void)
 {
 	glEnable(GL_DEPTH_TEST);
-	shaderProgram = rt3d::initShaders("minimal.vert", "minimal.frag");
-
+	shaderProgram = rt3d::initShaders("mvp.vert", "minimal.frag");
+	meshObjects[0] = testModel.ReadMD2Model("yoshi.md2");
+	md2VertCount = testModel.getVertDataCount();
 }
 
 void draw(SDL_Window* window)
@@ -49,7 +57,22 @@ void draw(SDL_Window* window)
 	//Clear the screen
 	glClearColor(0.5f, 0.5f, 0.5f, 1.0f);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+	glm::mat4 projection(1.0);
+	projection = glm::perspective(float(60.0f*DEG_TO_RADIAN), 800.0f / 600.0f, 1.0f, 500.0f);
 
+	glm::mat4 modelview(1.0);
+	mvStack.push(modelview);
+
+	testModel.Animate(0.016f);
+	rt3d::updateMesh(meshObjects[0], RT3D_VERTEX, testModel.getAnimVerts(), testModel.getVertDataSize());
+	//Draw yoshi
+	mvStack.push(mvStack.top());
+	mvStack.top() = glm::translate(mvStack.top(), glm::vec3(0.0f, 0.0f, -50.0f));
+	glm::mat4 mvp = projection * mvStack.top();
+	rt3d::setUniformMatrix4fv(shaderProgram, "MVP", glm::value_ptr(mvp));
+	rt3d::drawMesh(meshObjects[0], md2VertCount, GL_TRIANGLES);
+	mvStack.pop();
+	mvStack.pop();
 	SDL_GL_SwapWindow(window); // swap buffers
 }
 
@@ -62,7 +85,8 @@ int main(int argc, char* argv[])
 	SDL_Window * hWindow;
 	SDL_GLContext glContext;
 	hWindow = setupRC(glContext);
-
+	
+	
 	// Required on Windows *only* init GLEW to access OpenGL beyond 1.1
 	glewExperimental = GL_TRUE;
 	GLenum err = glewInit();
