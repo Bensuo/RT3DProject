92f35e9dad7c0974569310138f8711431c2e900c
Stuart Adams <stuartdadams@live.com>
2017-03-28 22:04:16 +0100

    Cleanup 1

--- RT3DProject/RT3DProject/UI.h
+++ RT3DProject/RT3DProject/UI.h
@@ -3,11 +3,9 @@
 #include <glm/glm.hpp>
 #include "rt3d.h"
 #include <SDL.h>
-#include <SDL_ttf.h>
-#include <iostream>
 #include "rt3dObjLoader.h"
 #include <string>
-#include <SDL_image.h>
+
 namespace Rendering 
 {
 	class UI : public IRenderable
@@ -15,13 +13,12 @@ namespace Rendering
 	public:
 		UI(std::string object, bool isText);
 		virtual ~UI();
-		// Inherited via IRenderable
-		virtual GLuint & getMesh() override;
-		virtual GLuint & getTexture() override;
-		virtual GLuint & getCount() override;
-		virtual rt3d::materialStruct & getMaterial() override;
-		virtual Transform & getTransform() override;
-		virtual bool isIndexed() override;
+		GLuint & getMesh() override;
+		GLuint & getTexture() override;
+		GLuint & getCount() override;
+		rt3d::materialStruct & getMaterial() override;
+		Transform & getTransform() override;
+		bool isIndexed() override;
 		void setText(std::string txt) { uiString = txt; }
 	private:
 		std::vector<GLfloat> verts;
@@ -29,9 +26,8 @@ namespace Rendering
 		std::vector<GLfloat> tex_coords;
 		std::vector<GLuint> indices;
 		std::string uiString;
-		GLuint health = 0;
 		GLuint label;
-		GLuint mesh[2];
+		GLuint mesh;
 		GLuint size;
 		bool isText = false;
 		SDL_Surface* texture;
@@ -41,10 +37,8 @@ namespace Rendering
 			{ 1.0f, 1.0f, 1.0f, 1.0f }, // specular
 			1.0f  // shininess
 		};
-		GLuint imageTexture(SDL_Surface* tex);
-		GLuint textToTexture(const char * str, GLuint textID);
-		void clearTextTexture(GLuint textID);
-		TTF_Font * textFont;
+		GLuint genTexture() const;
+		void clean(GLuint textID) const;
 		Transform transform{ glm::vec3(0), glm::vec3(0) };
 		std::string objPath = "res/obj/cube.obj";
 	};
