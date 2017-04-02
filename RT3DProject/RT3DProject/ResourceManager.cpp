709d7b001da851cb08f661a572d34cd68637c215
Bensuo <b00307589@studentmail.uws.ac.uk>
2017-04-02 18:47:07 +0100

    Moves loading of audio assets to resource manager, adds win/lose sfx

--- RT3DProject/RT3DProject/ResourceManager.cpp
+++ RT3DProject/RT3DProject/ResourceManager.cpp
@@ -53,4 +53,76 @@ namespace Utilities
 		}
 		return textureMap[filename];
 	}
+	Mix_Music * ResourceManager::loadMusic(std::string filename)
+	{
+		auto iterator = musicMap.find(filename);
+		Mix_Music* music = nullptr;
+		//if the sound has been loaded, play it
+		if (iterator != musicMap.end())
+		{
+			music = musicMap[filename];
+		}
+		//if the sound hasn't been loaded, load it and play it
+		else
+		{
+			auto newMusic = Mix_LoadMUS(filename.c_str());
+			if (newMusic == nullptr) {
+				printf("Failed to load sound effect! SDL_mixer Error: %s\n", Mix_GetError());
+				printf(filename.c_str(), "\n");
+				printf("\n");
+			}
+			else {
+				musicMap.emplace(filename, newMusic);
+				music = newMusic;
+			}
+		}
+		return music;
+	}
+	Mix_Chunk * ResourceManager::loadSound(std::string filename)
+	{
+		auto iterator = soundsMap.find(filename);
+		Mix_Chunk* chunk = nullptr;
+		//if the sound has been loaded, play it
+		if (iterator != soundsMap.end())
+		{
+			chunk = soundsMap[filename];
+		}
+		//if the sound hasn't been loaded, load it and play it
+		else
+		{
+			auto newSound = Mix_LoadWAV(filename.c_str());
+			if (newSound == nullptr) {
+				printf("Failed to load sound effect! SDL_mixer Error: %s\n", Mix_GetError());
+				printf(filename.c_str(), "\n");
+				printf("\n");
+			}
+			else {
+				soundsMap.emplace(filename, newSound);
+				chunk = newSound;
+			}
+		}
+		return chunk;
+	}
+	void ResourceManager::unloadAll()
+	{
+		//free the music
+		for (auto it = musicMap.begin(); it != musicMap.end(); ++it)
+		{
+			Mix_FreeMusic(it->second);
+		}
+		musicMap.clear();
+
+		//free the sounds
+		for (auto it = soundsMap.begin(); it != soundsMap.end(); ++it)
+		{
+			Mix_FreeChunk(it->second);
+		}
+		soundsMap.clear();
+
+		for (auto it = textureMap.begin(); it != textureMap.end(); it++)
+		{
+			glDeleteTextures(1, it->second.get());
+		}
+		textureMap.clear();
+	}
 }
--- RT3DProject/RT3DProject/ResourceManager.h
+++ RT3DProject/RT3DProject/ResourceManager.h
@@ -2,6 +2,7 @@
 #include <map>
 #include <memory>
 #include <GL/glew.h>
+#include <SDL_mixer.h>
 
 namespace Utilities
 {
@@ -11,7 +12,12 @@ namespace Utilities
 		ResourceManager();
 		virtual ~ResourceManager();
 		std::shared_ptr<GLuint> loadTexture(std::string filename);
+		Mix_Music* loadMusic(std::string filename);
+		Mix_Chunk* loadSound(std::string filename);
+		void unloadAll();
 	private:
 		std::map<std::string, std::shared_ptr<GLuint>> textureMap;
+		std::map<std::string, Mix_Music*> musicMap;
+		std::map<std::string, Mix_Chunk*> soundsMap;
 	};
 }
